6291d9b2abe2a78ed6a2e3940384cfd9946ced24
mauricio valdivia <ma.valdivia@mavi.mx>
2019-02-18 18:54:09 -0600

    scripts php

---
+++ php/ObtieneProductos.php
@@ -0,0 +1,32 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+require("conexion.php");
+$data = array();
+
+    $Obtiene = "select * from PRODUCTOS";
+    $Conexion;
+    $resultado=ibase_query($Obtiene);
+	$nr= ibase_num_fields($resultado);
+	if($nr>=1){
+        $data = array();
+        while($rows = ibase_fetch_object($resultado)) {
+            $datos = array(
+            "codigo" => $rows -> CODIGO,
+            "Descripcion" => $rows -> DESCRIPCION,
+            "Existencia" => $rows -> EXISTENCIA,
+            "Costo" => $rows -> COSTO
+            );
+        $data[] = $datos;
+        }
+        
+    }
+    else{
+        
+    }
+
+ibase_close($dbh);
+ header('Content-type: application/json; charset=utf-8');
+    echo json_encode($data);
+    exit();
+
+?>
---
+++ php/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$db = 'C:\Users\mavaldivia\Documents\Ionic\TEST.FDB';
+$username= 'SYSDBA';
+$password = 'masterkey';
+
+$dbh = ibase_connect($db, $username, $password);
+
+
+?>
